33a9933f544c48efd4f16ac27d473e300ac420a6
ΓΕΩΡΓΙΟΣ ΤΣΙΑΛΙΟΣ <up1072868@upnet.gr>
2023-05-29 20:30:32 +0300

    Changed if-else statement to ternary operator

--- gr.upatras.ask1/src/main/java/gr/upatras/ask1/Calculator.java
+++ gr.upatras.ask1/src/main/java/gr/upatras/ask1/Calculator.java
@@ -2,9 +2,10 @@ package gr.upatras.ask1;
 
 public class Calculator {
 	public static String subtract(int x, int y) {
-		if ((x - y) >= 0)
-			return "POSITIVE";
-		else
-			return "NEGATIVE";
+//		if ((x - y) >= 0)
+//			return "POSITIVE";
+//		else
+//			return "NEGATIVE";
+		return (x - y) >= 0 ? "POSITIVE" :"NEGATIVE";
 	} 
 } 
