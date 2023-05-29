ab8436fd6d2444b66e34bb70a6588c5442c3e6d0
ΓΕΩΡΓΙΟΣ ΤΣΙΑΛΙΟΣ <up1072868@upnet.gr>
2023-05-29 20:38:45 +0300

    Removed comments from subtract method

--- gr.upatras.ask1/src/main/java/gr/upatras/ask1/Calculator.java
+++ gr.upatras.ask1/src/main/java/gr/upatras/ask1/Calculator.java
@@ -2,10 +2,6 @@ package gr.upatras.ask1;
 
 public class Calculator {
 	public static String subtract(int x, int y) {
-//		if ((x - y) >= 0)
-//			return "POSITIVE";
-//		else
-//			return "NEGATIVE";
 		return (x - y) >= 0 ? "POSITIVE" :"NEGATIVE";
 	} 
-} 
+}  
